b047be0f7b40a31a85cd804d1a10a82c3ddf997c
Srinivas <sps21042003@gmail.com>
2024-09-10 07:05:53 +0530

    added basic register and login validations

--- client/src/app/Components/register/register.component.ts
+++ client/src/app/Components/register/register.component.ts
@@ -1,44 +1,44 @@
-import { Component, NgModule } from '@angular/core';
-import {
-  FormBuilder,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { AuthService } from '../../Services/auth.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule,CommonModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css',
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  
   registerForm: FormGroup;
 
   constructor(private fb: FormBuilder, private authService: AuthService) {
     this.registerForm = this.fb.group({
-      name: ['', [Validators.required]],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]],
-      address: ['', [Validators.required]],
-      contact: ['', [Validators.required]],
-      pincode: ['', [Validators.required]],
+      name: ['', [Validators.required, this.noWhitespaceValidator]],
+      email: ['', [Validators.required, Validators.email, this.noWhitespaceValidator]],
+      password: ['', [Validators.required, this.noWhitespaceValidator]],
+      address: ['', [Validators.required, this.noWhitespaceValidator]],
+      contact: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10), this.noWhitespaceValidator]],
+      pincode: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(6), this.noWhitespaceValidator]],
     });
   }
 
   onSubmit(): void {
     if (this.registerForm.valid) {
       const userData = this.registerForm.value;
-
       this.authService.register(userData).subscribe({
         next: (response) => {
           this.authService.setTokenAndNavigateToHome(response.token);
         },
       });
     }
+  }
+
+  // Custom validator to check for spaces
+  noWhitespaceValidator(control: AbstractControl): ValidationErrors | null {
+    const isWhitespace = (control.value || '').trim().length === 0;
+    const isValid = !isWhitespace;
+    return isValid ? null : { whitespace: true };
   }
 }
